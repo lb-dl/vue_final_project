a78c3f74bf249b3cda0b01ff1295f0808b5055ad
Irrishka K <irrishkak@MBP-Irrishka.home>
2023-03-04 14:30:37 +0100

    test vue-flask connection

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,27 @@
 <template>
-  <h1>Hello World</h1>
+  <h1>{{greeting}}</h1>
+  <li v-for="(user, index) in users" :key="index">
+    <ul>{{ user.id }}. {{user.firstname}} {{user.lastname}} - {{user.email}} {{user.created_at}}</ul>
+  </li>
+
 </template>
 
+<script>
+export default {
+
+  data: function(){
+    return {
+      greeting: 'Test Vue!',
+      users: []
+    }
+  },
+  created: async function(){
+    const response = await fetch("http://localhost:8000/api/v1/users");
+    this.users = await response.json();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
